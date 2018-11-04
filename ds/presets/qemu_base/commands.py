698e242ad45809d5eb5766af6f516a0d77b97a5e
Alex <alxgrmv@gmail.com>
2018-11-05 01:35:56 +0300

    qemu

---
+++ ds/presets/qemu_base/commands.py
@@ -0,0 +1,130 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from logging import getLogger
+
+from ds.command import Command
+from ds.utils.flatten import flatten
+
+
+logger = getLogger()
+
+
+class Help(Command):
+    short_help = 'Show all possible context modules'
+
+    def invoke_with_args(self, args):
+        pass
+
+
+class CreateDisk(Command):
+    def invoke_with_args(self, args):
+        pass
+
+
+class RunIso(Command):
+    usage = 'usage: {name} <iso> [<args>...]'
+
+    def invoke_with_args(self, args):
+        from pprint import pprint
+        pprint(args)
+        self.context.start(('-cdrom', args.get('<iso>'), args.get('<args>')))
+
+
+class Start(Command):
+    consume_all_args = True
+
+    def invoke_with_args(self, args):
+        self.context.executor.append(flatten((
+            self.context.qemu_bin,
+            self.context.get_run_options(),
+            args,
+        )))
+        if self.context.vnc:
+            self.context.viewer()
+
+
+class Viewer(Command):
+    def invoke_with_args(self, args):
+        display = self.context.qemu_environment. \
+            get('display')
+        assert display
+
+        address = '{host}:{display}'.format(host=self.context.vnc_host,
+                                            display=5900 + display)
+
+        self.context.executor.append(flatten((
+            self.context.viewer_bin,
+            address,
+        )))
+
+
+class UnixShell(Command):
+    def invoke_with_args(self, args):
+        shell = self.context.shell
+        if not shell:
+            logger.error('Context option `shell` is not defined')
+            return
+        self.context.connect_to(shell)
+
+
+class MonitorShell(Command):
+    def ensure_monitor(self):
+        if not self.context.monitor:
+            logger.error('Context option `monitor` is not defined')
+            return
+        return True
+
+    def invoke_with_args(self, args):
+        if not self.ensure_monitor():
+            return
+        self.context.connect_to(self.context.monitor)
+
+
+class Stop(MonitorShell):
+    def invoke_with_args(self, args):
+        if not self.ensure_monitor():
+            return
+        self.context.send_to(self.context.monitor, 'quit')
+
+
+class Reset(MonitorShell):
+    def invoke_with_args(self, args):
+        if not self.ensure_monitor():
+            return
+        self.context.send_to(self.context.monitor, 'system_reset')
+
+
+class Info(MonitorShell):
+    consume_all_args = True
+
+    def invoke_with_args(self, args):
+        if not self.ensure_monitor():
+            return
+        self.context.send_to(self.context.monitor, ' '.join([
+            'info',
+            ' '.join(args),
+        ]) + '\n')
+
+
+class Kill(MonitorShell):
+    def invoke_with_args(self, args):
+        self.context.executor.append(('ps', 'ax'))
+        result = self.context.executor.commit()
+        if result.code:
+            logger.error('Can\'t get a process list')
+            return
+
+        stdout = result.stdout.decode('utf-8').split('\n')
+        for item in stdout:
+            is_qemu = self.context.qemu_bin in item and \
+                      '-name {}'.format(self.context.name) in item
+            if not is_qemu:
+                continue
+
+            pid = item.split(' ', 1)[0]
+            self.context.executor.append(('kill', pid))
+            return
+
+        logger.error('Process is not found')
---
+++ ds/presets/qemu_base/mixins.py
@@ -0,0 +1,116 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import random
+from os.path import join
+
+from ds import context
+from ds.environment import PwdEnvironment
+from ds.utils.drop_empty import drop_empty
+
+
+class QemuEnvironment(PwdEnvironment):
+    env_filename = '.ds-qemu'
+
+
+class QemuEnvironmentMixin(context.Context):
+    def __init__(self, **options):
+        self._qemu_enviroment = None
+        super(QemuEnvironmentMixin, self).__init__(**options)
+
+    @property
+    def qemu_environment(self):
+        if self._qemu_enviroment is None:
+            self._qemu_enviroment = QemuEnvironment()
+        return self._qemu_enviroment
+
+
+class SocketOperateMixin(QemuEnvironmentMixin):
+    def connect_to(self, address):
+        protocol, address = address.split(':', 1)
+        address, opts = address.split(',', 1)
+        if protocol == 'unix':
+            self.executor.append(('rlwrap', 'nc', '-U', address))
+        elif protocol == 'tcp':
+            self.executor.append(['rlwrap', 'nc'] + address.split(':'))
+        else:
+            raise NotImplementedError
+
+    def send_to(self, address, data):
+        protocol, address = address.split(':', 1)
+        address, opts = address.split(',', 1)
+        data = data + '\n'
+        if protocol == 'unix':
+            self.executor.append(('nc', '-U', address), input=data)
+            self.executor.commit()
+        elif protocol == 'tcp':
+            self.executor.append(['nc'] + address.split(':'), input=data)
+            self.executor.commit()
+        else:
+            raise NotImplementedError
+
+    def format_address(self, protocol, path, **options):
+        return ':'.join([
+            protocol,
+            ','.join(drop_empty(*[
+                path,
+                ','.join([key for key, value in options.items() if value]),
+            ])),
+        ])
+
+    def format_unix(self, path, **options):
+        return self.format_address('unix', path, **options)
+
+    def format_tcp(self, path, **options):
+        return self.format_address('tcp', path, **options)
+
+
+class TcpSocketControlMixin(SocketOperateMixin):
+    @property
+    def monitor_address(self):
+        default = lambda: '127.0.0.1:{}'.format(random.randint(24000, 25000))
+        return self.qemu_environment.get('monitor-address', default=default)
+
+    @property
+    def monitor(self):
+        if not self.monitor_address:
+            return
+        return self.format_tcp(self.monitor_address, server=True, nowait=True)
+
+    @property
+    def shell_address(self):
+        default = lambda: '127.0.0.1:{}'.format(random.randint(24000, 25000))
+        return self.qemu_environment.get('shell-address', default=default)
+
+    @property
+    def shell(self):
+        if not self.shell_address:
+            return
+        nowait = not self.wait_shell
+        return self.format_tcp(self.shell_address, server=True, nowait=nowait)
+
+
+class UnixSocketControlMixin(SocketOperateMixin):
+    @property
+    def monitor_path(self):
+        default = join(self.project_root, '.qemu-monitor')
+        return self.qemu_environment.get('monitor-path', default=default)
+
+    @property
+    def monitor(self):
+        if not self.monitor_path:
+            return
+        return self.format_unix(self.monitor_path, server=True, nowait=True)
+
+    @property
+    def shell_path(self):
+        default = join(self.project_root, '.qemu-shell')
+        return self.qemu_environment.get('shell-path', default=default)
+
+    @property
+    def shell(self):
+        if not self.shell_path:
+            return
+        nowait = not self.wait_shell
+        return self.format_unix(self.shell_path, server=True, nowait=nowait)
