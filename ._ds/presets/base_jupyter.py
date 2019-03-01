cccb607ced1df08fd903ad5bd5c8d9e13404d896
Alex <alxgrmv@gmail.com>
2019-03-01 22:58:52 +0300

    .

--- ._ds/presets/base_jupyter.py
+++
@@ -1,9 +0,0 @@
-from python import PythonContext
-
-
-class JupyterContext(PythonContext):
-    """
-    https://jupyter-docker-stacks.readthedocs.io/
-    """
-
-    networks = 'host',
--- ._ds/presets/jupyter_datascience.py
+++
@@ -1,6 +0,0 @@
-from base_jupyter import JupyterContext
-from base_pull import Context as PullContext
-
-
-class Context(PullContext, JupyterContext):
-    default_image = 'jupyter/datascience-notebook'
